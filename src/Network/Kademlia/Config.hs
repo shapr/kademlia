1ce14531f1805bea0c769b3e91da8ad673bcfe10
Remy Goldschmidt <taktoa@gmail.com>
2018-09-20 18:06:59 -0500

    Refactored Network.Kademlia.Config

--- src/Network/Kademlia/Config.hs
+++ src/Network/Kademlia/Config.hs
@@ -1,70 +1,136 @@
+--------------------------------------------------------------------------------
+
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
+--------------------------------------------------------------------------------
+
+-- |
+-- Module      : Network.Kademlia.Config
+-- Description : Configuration parameters for a Kademlia instance.
+--
+-- FIXME: doc
+
+--------------------------------------------------------------------------------
+
 module Network.Kademlia.Config
-       ( KademliaConfig(..)
-       , WithConfigT (..)
-       , WithConfig
-       , getConfig
-       , usingConfigT
-       , usingConfig
-       , defaultConfig
-       , defaultRoutingSharingN
-       , defaultK
-       ) where
+  ( KademliaConfig (..)
+  , WithConfigT (..)
+  , WithConfig
+  , getConfig
+  , usingConfigT
+  , usingConfig
+  , defaultConfig
+  , defaultRoutingSharingN
+  , defaultK
+  ) where
 
-import           Control.Monad.Identity (Identity (runIdentity))
-import           Control.Monad.Reader   (ReaderT (..), ask)
+--------------------------------------------------------------------------------
+
+import           Control.Monad.Identity (Identity, runIdentity)
+import           Control.Monad.Reader   (ReaderT, ask, runReaderT)
 import           Control.Monad.Trans    (MonadTrans)
-import           Network.Kademlia.Utils (hour, minute)
+import           Network.Kademlia.Utils (minute)
 
+--------------------------------------------------------------------------------
+
+-- |
+-- This type encompasses all configuration parameters for a running Kademlia
+-- instance.
 data KademliaConfig
   = KademliaConfig
-    {
-      configK               :: !Int  -- ^ @k@ nearest heighbours for query. Constant from paper.
-    , configExpirationTime  :: !Int  -- ^ we delete a value after @expirationTime@ seconds has passed
-    , configStoreValueTime  :: !Int  -- ^ we store all values stored in the node in the 'k' closest known nodes every @storeValueTime@ seconds
-    , configPingTime        :: !Int  -- ^ we ping all known nodes every @pingTime@ seconds to make sure they are still present
-    , configNumLookupNodes  :: !Int  -- ^ number of nodes to look in parallel during a lookup
-                                     --   also known as α in kademlia paper
-    , configMsgSizeLimit    :: !Int  -- ^ upper bound on size of message transfered through
-                                     --   network; exceeding messages would be splitted
-    , configStoreValues     :: !Bool -- ^ when this is False, we don't store anything in this node
-    , configRoutingSharingN :: !Int  -- ^ number of nodes from not closest to include int `returnNodes` responses (see [CSL-260])
-    , configCacheSize       :: !Int  -- ^ cache size used by Node Storage Tree
-    , configPingLimit       :: !Int  -- ^ after unsuccessfull @pingLimit@ pings node will be throwed out from bucket
+    { configK               :: !Int
+      -- ^ Queries use the @k@ nearest heighbours; this is that @k@.
+      --   This is defined as a constant in the paper.
+      --
+      --   FIXME: this should be a type of positive numbers
+    , configPingTime        :: !Int
+      -- ^ This constant defines the period (in seconds) with which we ping all
+      --   known nodes to make sure they are still present.
+      --
+      --   FIXME: this should have higher resolution than seconds.
+      --
+      --   FIXME: this should be a type of positive numbers
+    , configNumLookupNodes  :: !Int
+      -- ^ The number of nodes to look in parallel during a lookup; this is
+      --   also known as α in the paper.
+      --
+      --   FIXME: this should be a type of positive numbers
+    , configMsgSizeLimit    :: !Int
+      -- ^ The upper bound on size of a message transfered through network;
+      --   messages larger than this will be split.
+      --
+      --   FIXME: what are the units?
+      --
+      --   FIXME: this should be a type of positive numbers
+    , configRoutingSharingN :: !Int
+      -- ^ The number of nodes from not closest to include in 'RETURN_NODES'
+      --   responses (see [CSL-260]).
+      --
+      --   FIXME: rewrite this doc comment
+    , configCacheSize       :: !Int
+      -- ^ The cache size used by node storage tree.
+      --
+      --   FIXME: what are the units?
+      --
+      --   FIXME: this should be a type of nonnegative numbers
+    , configPingLimit       :: !Int
+      -- ^ The number of pings after which an unresponsive node will be thrown
+      --   out from its bucket.
+      --
+      --   FIXME: this should be a type of positive numbers
     }
+  deriving (Eq, Show, Read)
 
-newtype WithConfigT m a = WithConfigT
-     { getWithConfigT :: ReaderT KademliaConfig m a
-     } deriving (Functor, Applicative, Monad, MonadTrans)
+--------------------------------------------------------------------------------
 
-type WithConfig a = WithConfigT Identity a
-
-getConfig :: Monad m => WithConfigT m KademliaConfig
-getConfig = WithConfigT ask
-
-usingConfigT :: WithConfigT m a -> KademliaConfig -> m a
-usingConfigT f cfg = flip runReaderT cfg $ getWithConfigT f
-
-usingConfig :: WithConfig a -> KademliaConfig -> a
-usingConfig f cfg = runIdentity $ usingConfigT f cfg
-
+-- | FIXME: doc
 defaultK :: Int
 defaultK = 7
 
+-- | FIXME: doc
 defaultRoutingSharingN :: Int
 defaultRoutingSharingN = uncurry (+) $ defaultK `divMod` 2
 
+-- | FIXME: doc
 defaultConfig :: KademliaConfig
-defaultConfig = KademliaConfig
+defaultConfig
+  = KademliaConfig
     { configK               = defaultK
-    , configExpirationTime  = hour 1
-    , configStoreValueTime  = hour 1
     , configPingTime        = minute 5
     , configNumLookupNodes  = 3
     , configMsgSizeLimit    = 1200
-    , configStoreValues     = True
     , configRoutingSharingN = defaultRoutingSharingN
     , configCacheSize       = 5
     , configPingLimit       = 4
     }
+
+--------------------------------------------------------------------------------
+
+-- |
+-- A monad transformer that gives access to a 'KademliaConfig'.
+newtype WithConfigT m a
+  = WithConfigT
+    { getWithConfigT :: ReaderT KademliaConfig m a
+    }
+  deriving (Functor, Applicative, Monad, MonadTrans)
+
+--------------------------------------------------------------------------------
+
+-- | FIXME: doc
+type WithConfig a = WithConfigT Identity a
+
+--------------------------------------------------------------------------------
+
+-- | FIXME: doc
+getConfig :: Monad m => WithConfigT m KademliaConfig
+getConfig = WithConfigT ask
+
+-- | FIXME: doc
+usingConfigT :: WithConfigT m a -> KademliaConfig -> m a
+usingConfigT f cfg = flip runReaderT cfg $ getWithConfigT f
+
+-- | FIXME: doc
+usingConfig :: WithConfig a -> KademliaConfig -> a
+usingConfig f cfg = runIdentity $ usingConfigT f cfg
+
+--------------------------------------------------------------------------------
